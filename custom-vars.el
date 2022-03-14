b6d494e960e8cfbd02d50ad3c4328f72481e5d93
Guillaume Portes <gportes@anotherplaceproductions.com>
2022-03-14 10:10:17 +0100

    custom vars file

---
+++ custom-vars.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(consult vertico yasnippet-snippets which-key visual-fill-column visual-fill use-package typescript-mode solidity-flycheck smex slime rainbow-delimiters pyvenv python-mode persp-projectile org-bullets multi-vterm magit lsp-ui lsp-ivy highlight-parentheses helpful git-gutter exec-path-from-shell evil-nerd-commenter eshell-git-prompt doom-themes doom-modeline dired-single dap-mode csharp-mode counsel-projectile company-solidity company-org-block company-box command-log-mode all-the-icons-ivy-rich all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
